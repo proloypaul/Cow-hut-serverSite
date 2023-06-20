a4575b93342159a72ec453f3bab8b79ed0a1f588
proloypaul <proloyp23@gmail.com>
2023-06-20 11:45:44 +0600

    database connected successfully with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 import app from './app';
+import config from './config';
 
-const port = 5000; 
+
 async function main() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/digital-cow-hut');
-    // await mongoose.connect(config.database_url as string);
+    // await mongoose.connect('mongodb://127.0.0.1:27017/digital-cow-hut');
+    await mongoose.connect(config.database_url as string);
 
     console.log('Database connected successfully');
 
-    app.listen(port, () => {
-      console.log(`University management server running port:  ${port}`);
+    app.listen(config.port, () => {
+      console.log(`University management server running port:  ${config.port}`);
     });
   } catch (err) {
     console.log('Failed to connect database ', err);
