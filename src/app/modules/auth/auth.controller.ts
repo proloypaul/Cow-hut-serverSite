364c24e284dc3aabdbf80a6b34937551991a054d
proloypaul <proloyp23@gmail.com>
2023-08-29 14:18:53 +0600

    set refreshtoken to our cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,17 +4,27 @@ import sendResponse from "../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
 import { IloginUserResponse } from "./auth.interface";
 import { AuthServices } from "./auth.service";
+import config from "../../../config";
 
 const loginUser = catchAsync(
     async(req: Request, res: Response) => {
         const {...loginData} = req.body;
         const result = await AuthServices.loginUserToDB(loginData);
 
+        const {refreshToken, ...others} = result
+
+        // set refreshToken to our cookies
+        const cookieOptions = {
+            secure: config.env === 'production',
+            httpOnly: true
+        };
+        res.cookie('refreshToken', refreshToken, cookieOptions)
+
         sendResponse<IloginUserResponse>(res, {
             statusCode: StatusCodes.OK,
             success: true,
             message: 'Login user successfully',
-            data: result
+            data: others
         });
     }
 )
